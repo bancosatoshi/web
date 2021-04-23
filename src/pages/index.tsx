88af8f45f3196ae2eff4841d963942ab9793f1b6
Gustavo Ibarra <gus@aufacicenta.com>
2021-04-23 09:09:26 -0600

    chore: adds og:graph tags and images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,30 @@
 import { NextPage } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
 import { Home } from "../app/Home/Home";
 
-const Index: NextPage = () => (
-  <>
-    <Head>
-      <title>Aufacicenta | Líderes en Estrategia DeFi</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link rel="stylesheet" href="https://use.typekit.net/wrf5blq.css" />
-      <meta
-        name="description"
-        content="Orientación especializada en Finanzas Descentralizadas (DeFi) para negocios de todos los tamaños de todas las regiones del mundo."
-      />
-    </Head>
-    <Home />
-  </>
-);
+const Index: NextPage = () => {
+  const { t } = useTranslation("home");
+  const { locale } = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>{t("head.og.title")}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet" href="https://use.typekit.net/wrf5blq.css" />
+        <meta name="description" content={t("head.og.description")} />
+        <meta property="og:title" content={t("head.og.title")} />
+        <meta property="og:description" content={t("head.og.description")} />
+        <meta property="og:image" content={`/shared/og-image_${locale}.png`} />
+      </Head>
+      <Home />
+    </>
+  );
+};
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
