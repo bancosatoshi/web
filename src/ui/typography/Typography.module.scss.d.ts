341e75e7a47db54c4d862d37553d0f26f3a381ee
Gustavo Ibarra <gus@aufacicenta.com>
2021-09-17 18:22:38 -0600

    chore: re-enables mixin

--- src/ui/typography/Typography.module.scss.d.ts
+++ src/ui/typography/Typography.module.scss.d.ts
@@ -1,19 +1,19 @@
 export type Styles = {
-  'typography': string;
-  'typography__button-label': string;
-  'typography__description': string;
-  'typography__headline1': string;
-  'typography__headline2': string;
-  'typography__headline3': string;
-  'typography__headline4': string;
-  'typography__headline5': string;
-  'typography__headline6': string;
-  'typography__link': string;
-  'typography__mini-button-label': string;
-  'typography__mini-description': string;
-  'typography__subtitle': string;
-  'typography__text': string;
-  'typography__text-bold': string;
+  typography: string;
+  "typography__button-label": string;
+  typography__description: string;
+  typography__headline1: string;
+  typography__headline2: string;
+  typography__headline3: string;
+  typography__headline4: string;
+  typography__headline5: string;
+  typography__headline6: string;
+  typography__link: string;
+  "typography__mini-button-label": string;
+  "typography__mini-description": string;
+  typography__subtitle: string;
+  typography__text: string;
+  "typography__text-bold": string;
 };
 
 export type ClassNames = keyof Styles;
