9d8c6a737a6a9177b0000b6d53b5da2195b4fefe
Gustavo Ibarra <gus@aufacicenta.com>
2021-09-19 23:24:33 -0600

    chore: next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const withTM = require("next-transpile-modules")([
+  "@solana/wallet-adapter-react",
+  "@solana/wallet-adapter-base",
+  "@solana/wallet-adapter-wallets",
+  "@solana/wallet-adapter-bitpie",
+  "@solana/wallet-adapter-blocto",
+  "@blocto/sdk",
+]);
+
 const { i18n } = require("./next-i18next.config");
 
-module.exports = {
+module.exports = withTM({
+  webpack5: false, // you want to keep using Webpack 4
   i18n,
-};
+});
