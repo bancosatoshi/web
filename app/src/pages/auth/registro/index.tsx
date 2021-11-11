881e475e51b1cdf11f1fd81b6d3545878c895dda
Gustavo Ibarra <gus@aufacicenta.com>
2021-11-11 17:26:28 -0600

    chore: adds typescript & sasspath aliases

--- app/src/pages/auth/registro/index.tsx
+++ app/src/pages/auth/registro/index.tsx
@@ -1,8 +1,8 @@
 import { NextPage } from "next";
 
-import { SignUpContainer } from "../../../app/auth/sign-up/SignUpContainer";
-import { AppLayout } from "../../../layouts/app-layout/AppLayout";
-import { AuthLayout } from "../../../layouts/auth-layout/AuthLayout";
+import { SignUpContainer } from "app/auth/sign-up/SignUpContainer";
+import { AppLayout } from "layouts/app-layout/AppLayout";
+import { AuthLayout } from "layouts/auth-layout/AuthLayout";
 
 const Index: NextPage = () => (
   <AppLayout>
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,9 +1,8 @@
 import { NextPage } from "next";
-import { Container } from "react-grid-system";
 
-import { HomeContainer } from "../app/home/home/HomeContainer";
-import { AppLayout } from "../layouts/app-layout/AppLayout";
-import { AuthLayout } from "../layouts/auth-layout/AuthLayout";
+import { HomeContainer } from "app/home/home/HomeContainer";
+import { AppLayout } from "layouts/app-layout/AppLayout";
+import { AuthLayout } from "layouts/auth-layout/AuthLayout";
 
 const Index: NextPage = () => (
   <AppLayout>
