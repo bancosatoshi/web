a43230b1658a7c40c539ab7847c3cce8de6b4752
Gustavo Ibarra <gus@aufacicenta.com>
2021-11-12 18:30:31 -0600

    chore: CMS cropped images height

--- app/src/app/business/BusinessDetails/BusinessContent/BusinessContent.tsx
+++ app/src/app/business/BusinessDetails/BusinessContent/BusinessContent.tsx
@@ -19,6 +19,14 @@ export const BusinessContent: React.FC<BusinessContentProps> = ({ className, con
           href="https://cms.bancosatoshi.com/wp-includes/css/dist/block-library/style.min.css?ver=5.8.2"
           media="screen"
         />
+        <style>
+          {`
+            .wp-block-image.alignfull img,
+            .wp-block-image.alignwide img {
+              height: auto;
+            }
+          `}
+        </style>
       </Head>
 
       <div
