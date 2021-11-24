e41e141dbb61788b4922ac30c59409a2f1d7a37c
Gustavo Ibarra <gus@aufacicenta.com>
2021-11-23 19:28:28 -0600

    feat: WIP btc-pay-server webhook InvoiceSettled

--- app/src/pages/api/webhooks/btc-pay-server/index.ts
+++ app/src/pages/api/webhooks/btc-pay-server/index.ts
@@ -1,24 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from "next";
-import crypto from "crypto";
-import databaseConnection from "src/providers/database";
+// import databaseConnection from "src/providers/database";
 
 import { BtcPayServerWebhookRequestBody } from "./btc-pay-server.types";
 
-const headerSignatureMatchesWebhookKey = (sig: string) => {
-  // @TODO we'll probably need to create a map of storeId => webhook_btcpay_sig because each store has its own webhook key
-  const serverKey = crypto.createHmac("sha256", process.env.WEBHOOKS_BTCPAY_SIG!).update("json").digest("base64");
-
-  // @TODO: The endpoint receiving the payload must validate the payload by checking that the HTTP header BTCPAY-SIG of the callback matches the HMAC256 of the secret on the payload's body bytes.
-  return true;
-};
+const headerSignatureMatchesWebhookKey = () => true;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { headers } = req;
-
-    if (!headerSignatureMatchesWebhookKey(headers["btcpay-sig"] as string)) {
+    if (!headerSignatureMatchesWebhookKey()) {
       throw new Error("api/webhooks/btc-pay-server: invalid BTCPAY_SIG");
     }
 
@@ -27,8 +18,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (!data || !data.invoiceId) {
       throw new Error("api/webhooks/btc-pay-server: invalid data. no invoice ID");
     }
-
-    const database = await databaseConnection.init();
 
     res.status(200).json({
       data,
