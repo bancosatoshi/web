9cdc4546d6bf34618ab25831d9d9a3609be12e9b
Kenneth Toxcon <kennethtoxro@gmail.com>
2021-11-24 10:52:22 -0600

    feat: active business campaigns grid (#30)
    * feat(business-card): adding 'BusinessCard' component foundation
    
    * feat(business-card): adding styles and props to business card
    
    * fix(business-card): refactoring business markup and styles
    
    * chore(business-campaign-card): renaming files and directory
    
    * feat(business-campaign-card): adding refactoring of 'business-card'
    
    * feat(lib/wordpress): adding business page content utility function
    
    * feat(db/business): adding new fields and tests
    
    * chore(business-campaign-card): updating types and props
    
    * feat(campaigns-grid): adding campaigns grid base structure
    
    * feat(api): adding graphql types and resolver for business campaigns
    
    * chore(api): removing debug log
    
    * feat(db/business): adding virtual fields
    
    * chore(api): adding suggested changes
    
    * chore(providers): adding new provider for wordpress
    
    * chore(campaigns-grid): adding suggestions and fixes
    
    * feat(campaigns-grid): adding toaster component
    
    * feat(campaigns-grid): adding improvements in CampaignsGroup and related
    
    * chore(Campaigns-Grid): removing strings concatenation
    
    * chore: couple of styling enhancements
    
    * fix: variant complains
    
    Co-authored-by: Gustavo Ibarra <gus@aufacicenta.com>

--- app/src/app/campaigns-grid/CampaignsGroup/CampaignsGroup.types.ts
+++ app/src/app/campaigns-grid/CampaignsGroup/CampaignsGroup.types.ts
@@ -1,6 +1,9 @@
-import { GetActiveBusinessCampaignsQuery } from "api/codegen";
+import { BusinessCampaign, GetActiveBusinessCampaignsQuery } from "api/codegen";
 
 export type CampaignsGroupProps = {
   className?: string;
+  singleLine?: boolean;
   campaigns: GetActiveBusinessCampaignsQuery["getActiveBusinessCampaigns"];
+  onCampaignClick: (...params: any) => void;
+  campaignsFilter?: (value: BusinessCampaign, index: number, array: BusinessCampaign[]) => BusinessCampaign[] | [];
 };
