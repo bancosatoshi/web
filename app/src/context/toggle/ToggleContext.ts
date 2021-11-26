289d5f68e8e38ddcf06b16ff9d5d604b582fa0fa
Kenneth Toxcon <kennethtoxro@gmail.com>
2021-11-25 21:02:48 -0600

    chore(dropdown): removing toggle context

--- app/src/context/toggle/ToggleContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { ToggleContextType } from "./ToggleContext.types";
-
-export const ToggleContext = createContext<ToggleContextType | undefined>(undefined);
