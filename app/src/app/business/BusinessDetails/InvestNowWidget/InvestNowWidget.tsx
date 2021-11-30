d6a31cbbbc2a3ef9d650c67fd4434667ee3e82cd
Gustavo Ibarra <gus@aufacicenta.com>
2021-11-30 02:18:44 -0600

    chore: updates InvestNowWidget structure

--- app/src/app/business/BusinessDetails/InvestNowWidget/InvestNowWidget.tsx
+++ app/src/app/business/BusinessDetails/InvestNowWidget/InvestNowWidget.tsx
@@ -63,24 +63,6 @@ export const InvestNowWidget: React.FC<InvestNowWidgetProps> = ({ className, cam
                 <Typography.Headline3 className={styles["invest-now-widget__cta-text"]}>
                   Invierte en {content.title}
                 </Typography.Headline3>
-                <div className={styles["invest-now-widget__terms"]}>
-                  <Grid.Row>
-                    <Grid.Col lg={6}>
-                      <Tooltip.Wrapper>
-                        <Typography.Headline4>1.5x</Typography.Headline4>
-                        <Tooltip
-                          title="ROI"
-                          description={`Si inviertes 10 SAT, ${content.title} se compromete a devolverte 15 SAT a lo largo de la vida útil del acuerdo: Nov 21, 2025.`}
-                        />
-                      </Tooltip.Wrapper>
-                      <Typography.Text>Retorno sobre tu inversión.</Typography.Text>
-                    </Grid.Col>
-                    <Grid.Col lg={6}>
-                      <Typography.Headline4>25 SAT</Typography.Headline4>
-                      <Typography.Text>Inversión mínima.</Typography.Text>
-                    </Grid.Col>
-                  </Grid.Row>
-                </div>
                 <div className={styles["invest-now-widget__cta-button"]}>
                   <Button fullWidth onClick={handleOnInvestNowClick} isLoading={isCheckoutLoading}>
                     {auth.hasActiveSession ? "Depositar BTC" : "Ingresa y Deposita BTC"}
@@ -93,6 +75,21 @@ export const InvestNowWidget: React.FC<InvestNowWidgetProps> = ({ className, cam
                   )}
                 </div>
                 <div className={styles["invest-now-widget__goal"]}>
+                  <div>
+                    <Tooltip.Wrapper>
+                      <Typography.Headline4>
+                        <Icon name="icon-bag-dollar" /> {campaign.investmentMultiple}x
+                      </Typography.Headline4>
+                      {/* @TODO get maturity_date formatted */}
+                      <Tooltip
+                        title="ROI"
+                        description={`Si inviertes 10 SAT, ${content.title} se compromete a devolverte ${
+                          10 * campaign.investmentMultiple
+                        } SAT a lo largo de la vida útil del acuerdo: Nov 21, 2025.`}
+                      />
+                    </Tooltip.Wrapper>
+                    <Typography.Description>Retorno sobre tu inversión</Typography.Description>
+                  </div>
                   <div>
                     <Typography.Headline4>
                       <Icon name="icon-bag-dollar" /> {campaign.totalSatsInvested} SAT
@@ -124,11 +121,13 @@ export const InvestNowWidget: React.FC<InvestNowWidgetProps> = ({ className, cam
                   </div>
                   <div>
                     <Typography.Description>
-                      <strong>Periodo de Retorno</strong>
+                      <strong>Periodo de Maduración</strong>
                     </Typography.Description>
+                    {/* @TODO get maturity_date formatted */}
                     <Typography.Description>
-                      Por ejemplo, si inviertes 10 SAT, {content.title} se compromete a devolverte 15 SAT durante la
-                      vida útil del acuerdo: <strong>Nov 21, 2025.</strong>
+                      Por ejemplo, si inviertes 10 SAT, {content.title} se compromete a devolverte{" "}
+                      {10 * campaign.investmentMultiple} SAT durante la vida útil del acuerdo:{" "}
+                      <strong>Nov 21, 2025.</strong>
                     </Typography.Description>
                   </div>
                 </div>
--- app/src/pages/api/business/resolvers/queries/getActiveBusinessCampaigns.ts
+++ app/src/pages/api/business/resolvers/queries/getActiveBusinessCampaigns.ts
@@ -1,6 +1,8 @@
 import { BusinessCampaign } from "api/codegen";
 import { QueryResolvers } from "api/codegen/resolvers-types";
 import { ResolversContext } from "api/graphql";
+import getTotalSatsInvested from "providers/database/getTotalSatsInvested";
+import getTotalInvestors from "providers/database/getTotalInvestors";
 
 import { getPageContentByBusinessCampaignSlug } from "providers/wordpress/getPageContentBySlug";
 
@@ -19,15 +21,26 @@ const getActiveBusinessCampaigns: QueryResolvers["getActiveBusinessCampaigns"] =
     const mergeCampaignDataWithWordpressContent = activeCampaigns.map(async (campaign) => {
       const slug = campaign.getDataValue("slug");
       const content = await getPageContentByBusinessCampaignSlug(slug);
+      const business_funding_campaign_plan_id = campaign.getDataValue("id");
+      const transactions = await database.dao.business_funding_campaign_transactions.findAllByBusinessFundingCampaignId(
+        {
+          business_funding_campaign_plan_id,
+        },
+      );
+
+      const totalInvestors = getTotalInvestors(transactions);
+      const totalSatsInvested = getTotalSatsInvested(transactions);
 
       return {
-        id: campaign.getDataValue("id"),
+        id: business_funding_campaign_plan_id,
         businessId: campaign.getDataValue("business_id"),
         expiresAt: campaign.getDataValue("expires_at"),
         slug,
-        totalSatsInvested: campaign.getDataValue("total_sats_invested"),
+        totalSatsInvested,
+        daysLeft: campaign.days_left,
         investmentMultiple: campaign.getDataValue("investment_multiple"),
         btcPayServerStoreId: campaign.getDataValue("btcpayserver_store_id"),
+        totalInvestors,
         content,
       } as BusinessCampaign;
     });
