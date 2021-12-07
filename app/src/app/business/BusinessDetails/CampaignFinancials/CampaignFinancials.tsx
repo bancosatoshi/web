44a43c5da46254ede772fb5a7cd14d2c4c78f1d4
Gus <gus@aufacicenta.com>
2021-12-07 15:36:29 -0600

    chore: adds data room use of funds chart. WIP #48 (#68)
    * chore: adds data room use of funds chart. WIP
    
    * chore: iframe width

--- app/src/app/business/BusinessDetails/CampaignFinancials/CampaignFinancials.tsx
+++ app/src/app/business/BusinessDetails/CampaignFinancials/CampaignFinancials.tsx
@@ -1,8 +1,11 @@
 import clsx from "clsx";
 
+import { Typography } from "ui/typography/Typography";
+
 import { CampaignFinancialsProps } from "./CampaignFinancials.types";
 import styles from "./CampaignFinancials.module.scss";
 import { FundingProgressBar } from "./FundingProgressBar/FundingProgressBar";
+import { IntendedUseOfFunds } from "./IntendedUseOfFunds/IntendedUseOfFunds";
 
 export const CampaignFinancials: React.FC<CampaignFinancialsProps> = ({ className, campaign }) => (
   <div className={clsx(styles["campaign-financials"], className)}>
@@ -11,5 +14,21 @@ export const CampaignFinancials: React.FC<CampaignFinancialsProps> = ({ classNam
       max={campaign.maxFundingInUSD}
       funded={campaign.totalSatsInvested}
     />
+
+    <IntendedUseOfFunds />
+
+    <Typography.Headline6>Financial Forecasts</Typography.Headline6>
+    <Typography.Text>...</Typography.Text>
+    <Typography.Headline6>Documents</Typography.Headline6>
+    <Typography.Text>...</Typography.Text>
+    <Typography.Headline6>Summary of Terms</Typography.Headline6>
+    <Typography.Text>...</Typography.Text>
+    <Typography.Headline5>Financial Condition</Typography.Headline5>
+    <Typography.Headline6>Operating History</Typography.Headline6>
+    <Typography.Text>...</Typography.Text>
+    <Typography.Headline6>Other outstanding debt or equity</Typography.Headline6>
+    <Typography.Text>...</Typography.Text>
+    <Typography.Headline5>Risk Factors</Typography.Headline5>
+    <Typography.Text>...</Typography.Text>
   </div>
 );
---
+++ app/src/app/business/BusinessDetails/CampaignFinancials/IntendedUseOfFunds/IntendedUseOfFunds.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+
+import { Typography } from "ui/typography/Typography";
+
+import { IntendedUseOfFundsProps } from "./IntendedUseOfFunds.types";
+import styles from "./IntendedUseOfFunds.module.scss";
+
+export const IntendedUseOfFunds: React.FC<IntendedUseOfFundsProps> = ({ className }) => (
+  <div className={clsx(styles["intended-use-of-funds"], className)}>
+    <Typography.Headline6>Intended Use of Funds</Typography.Headline6>
+    <iframe
+      width="100%"
+      height="371"
+      seamless
+      frameBorder="0"
+      title="use-of-funds"
+      scrolling="no"
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSYJ8HWsfZIzeDSTuery2meer1UWz3MGxUBn5LDxFV29vWUMtWrO7nwOmPJ7hIbfwsuQZxyOUk6CdzF/pubchart?oid=184349402&amp;format=interactive"
+    />
+  </div>
+);
